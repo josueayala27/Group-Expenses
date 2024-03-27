cf1f4807385a4adfc88696ea61b34e610aa59b13
Josué Ayala <ayalavalencia11@gmail.com>
2024-03-27 11:38:54 -0600

    Initial Commit

---
+++ Group Expenses/Group_ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group_ExpensesApp.swift
+//  Group Expenses
+//
+//  Created by Josue Ayala on 27/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Group_ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
