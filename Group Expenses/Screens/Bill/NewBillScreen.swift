12e1cadc8e3ab0c7126ebc4aa2cbc52e3d226856
Josué Ayala <ayalavalencia11@gmail.com>
2024-03-27 14:46:52 -0600

    Create split bills logic

--- Group Expenses/Screens/Bill/NewBillScreen.swift
+++ Group Expenses/Screens/Bill/NewBillScreen.swift
@@ -7,53 +7,76 @@
 
 import SwiftUI
 
-struct NewBillScreen: View {
+struct PeopleModel: Identifiable, Hashable {
+    var id: UUID = UUID()
+    var fullName: String
+    var amount: Decimal
+    var isIncluded: Bool
     
-    @State private var selectedNames: Set<String> = []
-    @State private var paidByModel: String?
+    //    Computed property
+    var formattedAmount: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "$"
+        return formatter.string(from: NSDecimalNumber(decimal: amount)) ?? ""
+    }
+}
+
+struct NewBillScreen: View {
     @State private var titleModel: String = ""
     @State private var quantityModel: Decimal?
-    @State private var names = [
-        "John Smith",
-        "Emma Johnson",
-        "Michael Williams",
-        "Sophia Brown"
+    @State private var people: [PeopleModel] = [
+        PeopleModel(fullName: "John Smith", amount: 0, isIncluded: true),
+        PeopleModel(fullName: "Emma Johnson", amount: 0, isIncluded: true),
+        PeopleModel(fullName: "Michael Williams", amount: 0, isIncluded: true),
+        PeopleModel(fullName: "Sophia Brown", amount: 0, isIncluded: true)
     ]
+    @State private var paidByModel: PeopleModel?
     
     let onCancel: () ->Void
+    
+    var splitBills: Void {
+        let totalOfIncludedPeople = people.filter({ $0.isIncluded }).count
+        
+        for index in people.indices {
+            if people[index].isIncluded {
+                people[index].amount = quantityModel! / Decimal(totalOfIncludedPeople)
+            }
+        }
+    }
     
     var body: some View {
         NavigationView {
             Form {
                 Section("Bill Details") {
                     TextField("Title", text: $titleModel)
-                    TextField("Quantity", value: $quantityModel, format: .currency(code: "USD")).keyboardType(.numberPad)
+                    
+                    TextField("Quantity", value: $quantityModel, format: .currency(code: "USD")).keyboardType(.numberPad).onSubmit {
+                        splitBills
+                    }
+                    
                     DatePicker(selection: .constant(Date()), displayedComponents: [.date], label: { Text("Date") }).datePickerStyle(.automatic)
+                    
                     Picker("Paid by", selection: $paidByModel) {
-                        ForEach(names, id: \.self) {
-                            name in Text("\(name)").tag(name)
+                        ForEach(people, id: \.self) {
+                            person in Text("\(person.fullName)").tag(person as PeopleModel).tag(person)
                         }
                     }
                 }
                 
                 Section("Split Between") {
-                    ForEach(names, id: \.self) { name in
+                    ForEach(Array(people.enumerated()), id: \.element) { index, person in
                         HStack {
-                            Image(systemName: self.selectedNames.contains(name) ? "checkmark.circle.fill" : "circle")
-                                .foregroundStyle(self.selectedNames.contains(name) ? Color.accentColor : .secondary)
-                                .accessibility(label: Text(self.selectedNames.contains(name) ? "Checked" : "Unchecked"))
+                            Image(systemName: person.isIncluded ? "checkmark.circle.fill" : "circle")
+                                .foregroundStyle(person.isIncluded ? Color.accentColor : .secondary)
                                 .imageScale(.large)
-                            Text(name)
+                            Text("\(person.fullName)")
                             Spacer()
-                            Text("$20.00")
+                            Text("\(person.formattedAmount)")
                         }
                         .contentShape(Rectangle())
                         .onTapGesture {
-                            if self.selectedNames.contains(name) {
-                                self.selectedNames.remove(name)
-                            } else {
-                                self.selectedNames.insert(name)
-                            }
+                            people[index].isIncluded.toggle()
                         }
                     }
                 }
